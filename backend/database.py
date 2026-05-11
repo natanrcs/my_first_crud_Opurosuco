0a3abffcec15f118f52a09a7cd02d528308f3948
est.natanrcs <est.natanrcs@JSMC02F659HQ05N.local>
2026-05-11 11:30:49 -0300

    Migracao do banco de dados,antes era um arquivinho agora passou a ser postgresql

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./meubanco.db"
+DATABASE_URL = "postgresql://admin:123456@host.docker.internal:5432/meubanco"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
