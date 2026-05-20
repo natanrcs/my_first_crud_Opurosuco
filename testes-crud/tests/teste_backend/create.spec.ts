d2828b3c63c67199f08f1f012aed9e8cd6355858
natanrcs <nathanrcs2022@gmail.com>
2026-05-20 14:51:07 -0300

    testes da API do backend feita,se quebrar eu arrumo

---
+++ testes-crud/tests/teste_backend/create.spec.ts
@@ -0,0 +1,12 @@
+import{test,expect, request} from "@playwright/test";
+
+test("Cadastrar produto via API BACKEND", async ({request}) => {
+    const response= await request.post("http://localhost:8001/products");
+    const dadosInserido={
+        name: "Blusa de Frio teste",price: 299,stok:20
+    }
+    expect(response.status()).toBe("201");
+    const corpodaResposta= await response.json()
+    expect(corpodaResposta.name).toBe("Blusa de frio teste");
+    expect(corpodaResposta.id).toBeDefined();
+});
