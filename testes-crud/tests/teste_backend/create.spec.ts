0bcc57676252d7fdfbc9770755abce31c23a5f3e
natanrcs <nathanrcs2022@gmail.com>
2026-05-21 11:51:19 -0300

    testes backend para testar API no metodo POST

--- testes-crud/tests/teste_backend/create.spec.ts
+++ testes-crud/tests/teste_backend/create.spec.ts
@@ -6,7 +6,7 @@ test("Cadastrar produto via API BACKEND", async ({request}) => {
         name: "Blusa de Frio teste",price: 299,stok:20
     }
     expect(response.status()).toBe("201");
-    const corpodaResposta= await response.json()
+    const corpodaResposta= await response.json();
     expect(corpodaResposta.name).toBe("Blusa de frio teste");
     expect(corpodaResposta.id).toBeDefined();
 });
