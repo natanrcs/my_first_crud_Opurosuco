991e223f8559478a0062690202af1bf3eca2adfa
natanrcs <nathanrcs2022@gmail.com>
2026-05-29 11:54:57 -0300

    Testes aprovados totalmente funcional

--- testes-crud/tests/read.spec.ts
+++ testes-crud/tests/read.spec.ts
@@ -4,7 +4,7 @@ async function listarProduto(page:Page,readUrl:string) {
     await page.goto(readUrl);
     await expect(page.getByText("List Products: ")).toBeVisible();
     await page.getByRole("button",{name: "Listar"}).click();
-    await expect(page.getByText("Shorts Oakley")).toBeVisible();
+    await expect(page.getByText("Shorts Billabong")).toBeVisible();
     await expect(page.getByText("Preço: 200")).toBeVisible();
     await expect(page.getByText("Estoque: 200")).toBeVisible();
     await page.getByRole("button",{name:"Zerar"}).click();
--- testes-crud/tests/update.spec.ts
+++ testes-crud/tests/update.spec.ts
@@ -1,13 +1,13 @@
 import{test,expect,Page} from "@playwright/test";
 const updateUrl="http://localhost:8001/update.html";
 async function updateProduto(page:Page,updateUrl:string){
-    page.on("dialog", async dialog => {
+    page.once("dialog", async dialog => {
         expect(dialog.message()).toBe("Id atualizado com sucesso!");
         await dialog.accept();
     })
     await page.goto(updateUrl);
     await page.getByPlaceholder("ID do produto").fill("1");
-    await page.getByPlaceholder("Novo nome do produto").fill("Shorts Oakley");
+    await page.getByPlaceholder("Novo nome do produto").fill("Shorts Billabong");
     await page.getByPlaceholder("Novo preço").fill("200");
     await page.getByPlaceholder("Novo estoque").fill("200");
     await page.getByRole("button",{name: "Atualizar"}).click();
